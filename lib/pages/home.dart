1f36c06267d8fd687e5af1723be4dfb5ac119671
Manny Singh <likebeats@gmail.com>
2023-10-29 17:27:00 -0400

    add loads view & settings view

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:carrier_nest_flutter/rest/loads.dart';
-import 'package:carrier_nest_flutter/constants.dart';
-import 'package:carrier_nest_flutter/models.dart';
-import 'package:carrier_nest_flutter/pages/load_details.dart';
-import 'package:carrier_nest_flutter/pages/driver_login.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:carrier_nest_flutter/pages/loads_view.dart';
+import 'package:carrier_nest_flutter/pages/settings_view.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -14,105 +10,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<Map<String, dynamic>>? _loadsFuture;
+  int _currentIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _fetchLoads();
-  }
+  // Widget for Loads view
+  final Widget _loadsView = LoadsView();
 
-  Future<void> _fetchLoads() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? jwtToken = prefs.getString('jwtToken');
-    if (jwtToken == null) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => DriverLoginPage()),
-      );
-      return;
-    }
-    String? driverId = prefs.getString('driverId');
+  // Widget for Settings view
+  final Widget _settingsView = SettingsView();
 
-    _loadsFuture = Loads.getLoadsExpanded(
-            expand: 'customer,shipper,receiver',
-            sort: Sort(key: 'refNum', order: 'desc'),
-            limit: 10,
-            offset: 0,
-            driverId: driverId)
-        .catchError((error) {
-      // Handle errors here
-      print("Error fetching loads: $error");
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
     });
-
-    // Call setState if needed
-    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My Loads"),
+        title: Text('Carrier Nest'),
       ),
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: _loadsFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (snapshot.hasData && snapshot.data!['loads'].isNotEmpty) {
-            List<ExpandedLoad> loads = snapshot.data!['loads'];
-            return ListView.builder(
-              itemCount: loads.length,
-              itemBuilder: (context, index) {
-                ExpandedLoad load = loads[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LoadDetailsPage(
-                          loadId: load.id,
-                        ),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    margin: const EdgeInsets.all(8.0),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(load.customer.name,
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold)),
-                          const SizedBox(height: 8.0),
-                          Text('Load/Order #: ${load.refNum}',
-                              style: const TextStyle(fontSize: 16)),
-                          const SizedBox(height: 8.0),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                  '${load.shipper.city}, ${load.shipper.state} to ${load.receiver.city}, ${load.receiver.state}'),
-                            ],
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text('Pickup Date: ${load.shipper.date.toString()}'),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
-          } else {
-            return const Center(child: Text('No loads found'));
-          }
-        },
+      body: _currentIndex == 0 ? _loadsView : _settingsView,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.local_shipping),
+            label: 'Loads',
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
---
+++ lib/pages/settings_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:carrier_nest_flutter/rest/+dio_client.dart';
+import 'package:carrier_nest_flutter/pages/driver_login.dart';
+
+class SettingsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<SharedPreferences>(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    'Phone Number: ${snapshot.data!.getString('phoneNumber')}'),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    await DioClient().clearCookies();
+                    await DioClient().clearPreferences();
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DriverLoginPage()),
+                    );
+                  },
+                  child: const Text('Logout'),
+                ),
+              ],
+            ),
+          );
+        } else {
+          return const Center(child: Text('No data found'));
+        }
+      },
+    );
+  }
+}
