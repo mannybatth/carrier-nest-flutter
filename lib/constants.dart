d54f0db32b7dc6c551bcf847dde3dcd711867ef5
likebeats <likebeats@gmail.com>
2025-03-01 23:23:27 +0000

    pub get

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-const apiUrl = kReleaseMode ? 'https://carriernest.com/api' : 'http://192.168.86.32:3000/api';
+const apiUrl = kReleaseMode ? 'https://carriernest.com/api' : 'https://9000-idx-carrier-nest-web-1740863210242.cluster-fnjdffmttjhy2qqdugh3yehhs2.cloudworkstations.dev/api';
 
 class JSONResponse<T> {
   final T data;
