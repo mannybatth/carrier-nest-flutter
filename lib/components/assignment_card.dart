bf9ceed693b3463260e4d21eb849d27ba6d6f7a9
Sandeep <sandeeppenaich@outlook.com>
2025-05-07 21:41:18 -0700

    Enhance AssignmentCard layout and add completed status display

--- lib/components/assignment_card.dart
+++ lib/components/assignment_card.dart
@@ -1,5 +1,7 @@
+import 'package:carrier_nest_flutter/helpers/helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:carrier_nest_flutter/models.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:intl/intl.dart';
 
 class AssignmentCard extends StatelessWidget {
@@ -11,29 +13,59 @@ class AssignmentCard extends StatelessWidget {
   Widget build(BuildContext context) {
     ExpandedLoad load = assignment.load!;
     RouteLeg routeLeg = assignment.routeLeg!;
+    // check load status
+    bool completed = routeLeg.status == RouteLegStatus.COMPLETED;
 
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      padding: const EdgeInsets.all(0.0),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(12.0),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            offset: Offset(0, 4),
-            blurRadius: 12.0,
-          ),
-        ],
+        border: Border.all(
+          color: completed ? Colors.grey.shade200 : Colors.white,
+          width: 2.0,
+        ),
+        boxShadow: completed
+            ? null
+            : const [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 4),
+                  blurRadius: 8.0,
+                ),
+              ],
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           _buildHeader(load.refNum, load.customer.name),
-          const SizedBox(height: 12),
-          _buildRouteInfo(routeLeg),
-          const SizedBox(height: 12),
-          _buildScheduleInfo(routeLeg),
+          const SizedBox(height: 8),
+          Padding(padding: const EdgeInsets.fromLTRB(16, 4, 16, 4), child: _buildRouteInfo(routeLeg)),
+          const SizedBox(height: 8),
+          Padding(padding: const EdgeInsets.fromLTRB(16, 4, 16, 12), child: _buildScheduleInfo(routeLeg)),
+          completed
+              ? Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                  child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.green.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      child: const Text(
+                        'Completed',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ]),
+                )
+              : const SizedBox(),
         ],
       ),
     );
@@ -42,23 +74,75 @@ class AssignmentCard extends StatelessWidget {
   Widget _buildHeader(String refNum, String customerName) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
       children: [
-        Text(
-          refNum,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 0),
+          child: Text(
+            'ORDER# $refNum',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w800,
+              color: Colors.grey.shade800,
+            ),
           ),
         ),
-        const SizedBox(height: 4),
-        Text(
-          customerName,
-          style: TextStyle(
-            fontSize: 14,
-            color: Colors.grey[600],
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 0, bottom: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const HeroIcon(HeroIcons.truck, color: Colors.grey, size: 16),
+              const SizedBox(width: 6),
+              Text(
+                assignment.routeLeg?.distanceMiles != null ? '${assignment.routeLeg!.distanceMiles.toStringAsFixed(2)} miles' : '0',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[600],
+                ),
+              ),
+              const SizedBox(width: 12),
+              const Icon(Icons.timelapse_outlined, color: Colors.grey, size: 16),
+              const SizedBox(width: 6),
+              Text(
+                //_routeLeg?.durationHours != null ? hoursToReadable(_routeLeg!.durationHours) : '0'
+                assignment.routeLeg?.durationHours != null ? '${hoursToReadable(assignment.routeLeg?.durationHours ?? 0)} ' : '0',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[600],
+                ),
+              ),
+            ],
           ),
-        ),
+        )
+      ],
+    );
+  }
+
+  // check to see if routeleg loctions has more than 2 locations
+  // if so, build array of _buildLocationText and return it as array of widgets
+  // else, build _buildLocationText with the first and last location
+
+  Widget _buildStops(RouteLeg routeLeg) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        for (int i = 0; i < routeLeg.locations.length; i++)
+          _buildLocationTextInColumn(
+            title: i == 0
+                ? 'From'
+                : i == routeLeg.locations.length - 1
+                    ? 'To'
+                    : 'Stop',
+            location: _getLocationName(routeLeg.locations[i]).toLowerCase(),
+            city: _getLocationCity(routeLeg.locations[i]).toLowerCase(),
+            state: _getLocationState(routeLeg.locations[i]).toLowerCase(),
+            index: i,
+            length: routeLeg.locations.length,
+          ),
       ],
     );
   }
@@ -66,26 +150,10 @@ class AssignmentCard extends StatelessWidget {
   Widget _buildRouteInfo(RouteLeg routeLeg) {
     return Row(
       children: [
-        const Icon(Icons.location_on, color: Colors.blueAccent, size: 20),
-        const SizedBox(width: 8),
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildLocationText(
-                title: 'From',
-                location: _getLocationName(routeLeg.locations.first),
-                city: _getLocationCity(routeLeg.locations.first),
-                state: _getLocationState(routeLeg.locations.first),
-              ),
-              const SizedBox(height: 4),
-              _buildLocationText(
-                title: 'To',
-                location: _getLocationName(routeLeg.locations.last),
-                city: _getLocationCity(routeLeg.locations.last),
-                state: _getLocationState(routeLeg.locations.last),
-              ),
-            ],
+            children: [_buildStops(routeLeg)],
           ),
         ),
       ],
@@ -115,6 +183,76 @@ class AssignmentCard extends StatelessWidget {
     );
   }
 
+  // Eumm to hold stop type "From", "To", "Stop"
+  Widget _getIconBasedOnLocationType(int index, int length) {
+    if (index == 0) {
+      return Icon(Icons.location_on, color: Colors.blueAccent.shade200, size: 20);
+    } else if (index == length - 1) {
+      return Icon(Icons.location_on, color: Colors.red.shade400, size: 20);
+    } else {
+      return Icon(Icons.circle, color: Colors.grey.shade400, size: 20);
+    }
+  }
+
+  Widget _buildLocationTextInColumn(
+      {required String title,
+      required String location,
+      required String city,
+      required String state,
+      required int index,
+      required int length}) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _getIconBasedOnLocationType(index, length),
+        const SizedBox(width: 2),
+        SizedBox(
+          width: 60,
+          child: Text(
+            title.toUpperCase(),
+            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 14, color: Colors.grey),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.only(left: 8),
+            margin: const EdgeInsets.only(bottom: 8),
+            decoration: const BoxDecoration(
+              border: Border(
+                left: BorderSide(
+                  color: Colors.grey,
+                  width: 3.0,
+                ),
+              ),
+              color: Color.fromARGB(255, 245, 245, 245),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 4),
+                Text(
+                  location.toUpperCase(),
+                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(height: 0),
+                // Capitalize first letter of city and uppercase state
+                Text(
+                  '${city[0].toUpperCase()}${city.substring(1)}, ${state.toUpperCase()}',
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color.fromARGB(255, 76, 76, 76),
+                  ),
+                ),
+                const SizedBox(height: 4),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
   Widget _buildScheduleInfo(RouteLeg routeLeg) {
     return Row(
       children: [
@@ -124,13 +262,16 @@ class AssignmentCard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
-              'Scheduled',
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+              'Scheduled At',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+              ),
             ),
             const SizedBox(height: 2),
             Text(
-              '${_formatDate(routeLeg.scheduledDate!)} at ${routeLeg.scheduledTime}',
-              style: TextStyle(color: Colors.grey[600], fontSize: 14),
+              '${_formatDate(routeLeg.scheduledDate!)} at ${DateFormat("hh:mm a").format(DateFormat("HH:mm").parse(routeLeg.scheduledTime))}',
+              style: TextStyle(color: Colors.grey[800], fontSize: 14),
             ),
           ],
         ),
